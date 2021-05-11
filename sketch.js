70ceffd4152e81c2085b0570795b1b1f2da77917
SebastianRojas748 <78050856+SebastianRojas748@users.noreply.github.com>
2021-05-11 14:27:05 -0700

    2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 /***********************************************************************************
-  CityAI
+  SFAI
   by Sebastian Rojas
 
   Uses the p5.2DAdventure.js class 
@@ -91,11 +91,11 @@ let bodyFont;
 // Allocate Adventure Manager with states table and interaction tables
 function preload() {
 
-  headlineFont = loadFont('fonts/Montserrat-Thin.otf');
-  bodyFont = loadFont('fonts/Montserrat-Thin.otf');
+  headlineFont = loadFont('fonts/Montserrat-Regular.otf');
+  bodyFont = loadFont('fonts/Montserrat-Regular.otf');
 
   // load all images
-  barImage = loadImage("assets/anger_emoji.png");
+  barImage = loadImage("assets/bar.png");
   
   allocateCharacters();
 
@@ -243,41 +243,70 @@ clDodge_the_lawsuit = function() {
   adventureManager.clickablePressed(this.name);
 }
 
-clIgnoreThem = function() {
-  characters[nimby].addAnger(1);
-  characters[treeHugger].addAnger(1);
-  characters[bigLabor].addAnger(1);
+clPay_the_7_million = function() {
+  characters[Residents].addbar(4);
   adventureManager.clickablePressed(this.name);
 }
 
-clCutArts = function() {
-  characters[treeHugger].addAnger(2);
-  characters[consumer].addAnger(2);
-  characters[mayor].addAnger(1);
+clBlame_the_AI2 = function() {
+  characters[AI].addbar(1);
+  characters[Unions].subbar(1);
   adventureManager.clickablePressed(this.name);
 }
 
-clCutTransportation = function() {
-  characters[treeHugger].addAnger(3);
-  characters[mayor].addAnger(1);
-  characters[consumer].addAnger(1);
+clPay_for_their_new_plane_tickets = function() {
+  characters[Tourists].subbar(2);
+  characters[Residents].addbar(2);
   adventureManager.clickablePressed(this.name);
 }
 
-clCutCityWages = function() {
-  characters[mayor].addAnger(2);
-  characters[bigLabor].addAnger(2);
-  characters[consumer].addAnger(1);
+clApologize_and_assure_there_are_no_errors = function() {
+  characters[Residents].subbar(2);
+  characters[Tourists].addbar(2);
   adventureManager.clickablePressed(this.name);
 }
 
-clCutParks = function() {
-  characters[mayor].addAnger(1);
-  characters[treeHugger].addAnger(2);
-  characters[consumer].addAnger(1);
+clBlame_the_AI3 = function() {
+  characters[AI].addbar(1);
+  characters[Unions].subbar(1);
   adventureManager.clickablePressed(this.name);
 }
 
+clPay_for_damages = function() {
+  characters[Residents].addbar(3);
+  characters[Unions].subbar(3);
+  adventureManager.clickablePressed(this.name);
+}
+
+clDodge_the_lawsuit_with_city_lawyers = function() {
+  characters[Unions].addbar(3);
+  characters[Residents].subbar(3);
+  adventureManager.clickablePressed(this.name);
+}
+
+clBlame_the_AI4 = function() {
+  characters[AI].addbar(1);
+  characters[Unions].subbar(1);
+  adventureManager.clickablePressed(this.name);
+}
+
+clCompensate_them_for_their_troubles = function() {
+  characters[Residents].subbar(2);
+  characters[Tourists].subbar(2);
+  adventureManager.clickablePressed(this.name);
+}
+
+clIssue_a_Public_statement_and_apology = function() {
+  characters[Residents].subbar(2);
+  characters[Unions].subbar(2);
+  adventureManager.clickablePressed(this.name);
+}
+
+clBlame_the_AI5 = function() {
+  characters[AI].addbar(1);
+  characters[Unions].subbar(1);
+  adventureManager.clickablePressed(this.name);
+}
 
 
 
@@ -285,12 +314,10 @@ clCutParks = function() {
 //-------------- CHARACTERS -------------//
 function allocateCharacters() {
   // load the images first
-  characterImages[goomazon] = loadImage("assets/goomazon.jpg");
-  characterImages[mayor] = loadImage("assets/mayor.jpg");
-  characterImages[bigLabor] = loadImage("assets/bigLabor.jpg");
-  characterImages[nimby] = loadImage("assets/nimby.jpg");
-  characterImages[treeHugger] = loadImage("assets/treeHugger.jpg");
-  characterImages[consumer] = loadImage("assets/consumer.jpg");
+  characterImages[Residents] = loadImage("assets/Resident.png");
+  characterImages[Tourists] = loadImage("assets/Tourist.png");
+  characterImages[Unions] = loadImage("assets/Union.png");
+  characterImages[AI] = loadImage("assets/AI.png");
 
   for( let i = 0; i < characterImages.length; i++ ) {
     characters[i] = new Character();
@@ -298,10 +325,10 @@ function allocateCharacters() {
   }
 
   // default anger is zero, set up some anger values
-  characters[bigLabor].addAnger(1);
-  characters[nimby].addAnger(2);
-  characters[treeHugger].addAnger(1);
-  characters[consumer].subAnger(2); // test
+  characters[Residents].addbar(1);
+  characters[Tourists].addbar(1);
+  characters[Unions].addbar(5);
+  characters[AI].subbar(1); // test
 }
 
 class Character {
@@ -315,7 +342,7 @@ class Character {
     this.image = img;
     this.x = x;
     this.y = y;
-    this.anger = 0;
+    this.bar = 0;
   }
 
   draw() {
@@ -326,32 +353,32 @@ class Character {
       image( this.image, this.x, this.y );
 
       // draw anger emojis
-      for( let i = 0; i < this.anger; i++ ) {
-        image(angerImage, this.x + 70 + (i*40), this.y +10 );
+      for( let i = 0; i < this.bar; i++ ) {
+        image(barImage, this.x + 70 + (i*40), this.y +10 );
       }
 
       pop();
     }
   }
 
-  getAnger() {
-    return this.anger;
+  getbar() {
+    return this.bar;
   }
 
   // add, check for max overflow
-  addAnger(amt) {
-    this.anger += amt;
-    if( this.anger > maxAnger ) {
-      this.anger = maxAnger;
+  addbar(amt) {
+    this.bar += amt;
+    if( this.bar > maxbar ) {
+      this.bar = maxbar;
     }
 
   }
 
   // sub, check for below zero
-  subAnger(amt) {
-    this.anger -= amt;
-    if( this.anger < 0 ) {
-      this.anger = 0;
+  subbar(amt) {
+    this.bar -= amt;
+    if( this.bar < 0 ) {
+      this.bar = 0;
     }
   }
 }
@@ -367,14 +394,33 @@ function loadAllText() {
 // copy the array reference from adventure manager so that code is cleajer
   scenarioRooms = adventureManager.states;
 
-  scenarioRooms[startScreen].setText("Who Pays for it?", "The underground tunnels cost money to maintain. Goomazon threatens to leave the city if they have to pay for all the maintenance work. Who pays for it?");
-  scenarioRooms[goomazonMovesScreen].setText("Do we lure them back?", "Goomazon moves their headquarters to our rival city across the river. They layoff local workers. How should we respond?");
-  scenarioRooms[cityPaysScreen].setText("What do we cut?", "The city budget is getting tanked because of the cost of the tunels. Which programs should we cut?");
-  scenarioRooms[raisedTaxesScreen].setText("How do we help the economy?", "The wealthy leave the city in droves. Restaurants start closing and our tax base is depleted. What do we do?");
-  scenarioRooms[rivalCompanyScreen].setText("It's bad, what do we do?", "The rival company is even worse than Goomazon. In addition to being anti-union, they force everyone to wear silly uniforms, sing happy children's songs and sign the most restrictive NDAs ever.");
-  scenarioRooms[goomazonExpands].setText("Oh-no! Now what to do?", "Goomazon expands its operations. It is now both in your city and the rival city. It's driven out all the local businesses.");
-  scenarioRooms[cityUgly].setText("How can we fix this?", "The city has cut the budget to some of its essential services. It's been a cascading effect. Without arts and adequate transportation, everyone has become depressed. THE END.");
-  scenarioRooms[workersStrike].setText("How do we respond?", "There are massive worker's strikes. The city is shut down. Big labor is angry and riling people up. Thousands of protesters are in the streets.");
+  scenarioRooms[Problem1].setText("An AI tourist bus has been hacked by a University Computer science major, the tourists want answers for this incident");
+  scenarioRooms[Compensate_the_Tourists].setText("Having allocated the funds from taxpayers, the tourists are satisfied, but the residents are not. Score: +10");
+  scenarioRooms[ReAssure_the_City].setText("The tourists are dissatisfied having not been compensated, but the residents feel somewhat safer. Score: +5");
+  scenarioRooms[Blame_the_AI1].setText("The programmers found the missing  ending bracket and the AI does not like being blamed. The AI will remember that. Score: -10");
+  scenarioRooms[Problem2].setText("A resident using an AI taxi has been accidently charged 7 million dollars for their ride, she demands an answer from the city");
+  scenarioRooms[Dodge_the_lawsuit].setText("This resident is outraged and trust with city residents has plummeted. Score: +1000");
+  scenarioRooms[Pay_the_7_million].setText("The residents are outraged as their taxpayer money is mishandled. Score: +100");
+  scenarioRooms[Blame_the_AI2].setText("The programmers found the problem in the decimal placing for the charge function. The AI does not like being blamed. The AI will remember that. Score: -100");
+  scenarioRooms[Problem3].setText("An AI airliner originally headed for Beijing, China has instead gone to Kyoto, Japan. The passengers are furious and want answers");
+  scenarioRooms[Pay_for_their_new_plane_tickets].setText("The residents are outraged as their taxpayer money is mishandled. Score: +Zero");
+  scenarioRooms[Apologize_and_assure_there_are_no_errors].setText("The Tourists are enraged as they find out they are not being compensated. Score: +Ten");
+  scenarioRooms[Blame_the_AI3].setText("The programmers found the problem was that the original programmer confused Japanese and Chinese characters. The AI does not like being blamed. Score: -Twenty");
+  scenarioRooms[Problem4].setText("A Delivery Truck has been rear-ended by an AI Bus, The Truck driver is furious that their is no bus driver to yell at, he wants answers from the city");
+  scenarioRooms[Pay_for_damages].setText("The residents are outraged as their taxpayer money is mishandled. Score: +A");
+  scenarioRooms[Dodge_the_lawsuit_with_city_lawyers].setText("The Trucker Unions are outraged and begin lobbying harder against AI vehicles. Score: +b");
+  scenarioRooms[Blame_the_AI4].setText("The programmers found nothing wrong in the coding. the incident is entirely the truckers fault, nonetheless the AI is blamed for PR sake. The AI will remember that. Score: -Z");
+  scenarioRooms[Problem5].setText("An AI  BART train is not registering women with short hair as Passengers and is refusing to operate. These women have created Women against AI and demand answers");
+  scenarioRooms[Compensate_them_for_their_troubles].setText("The women are enraged as they refuse to be bought off. The AI and City officials are being labelled sexists. Score: +Sexism");
+  scenarioRooms[Issue_a_Public_statement_and_apology].setText("The Women against AI group finds support with public transport and tucker unions and begin protesting for the dismantling of the AI system. Score: +Angry");
+  scenarioRooms[Blame_the_AI5].setText("The programmers found that the original programmer inserted their personal bias and created Var woman == long hair. The AI is blamed for PR sake. The AI will remember that. Score: +Death");
+  scenarioRooms[Conclusion].setText("After only 5 days of the City AI implementation ...");
+  scenarioRooms[Ending1].setText("The City has run its budget to the ground and is now in severe debt. Having to dismantle the AI system, the residents lament as they have to return to the primitive pre-AI transportation");
+  scenarioRooms[Ending2].setText("Although the City AI has its occasional glitches and bugs, its decided that it is just functional enough to keep. However public image and overall trust in the city is at an all-time low.");
+  scenarioRooms[Ending3].setText("Despite the problems and stigmas associated with the AI system, it is seen as too valuable to remove, and instead plans are being made to expand to other cities across America");
+  scenarioRooms[Ending4].setText("It seems that the AI code needs some updates here and there, eventually the rough edges will be rounded out. aside from that, legislation has begun to implement this AI to all personal vehicles too");
+  scenarioRooms[Ending5].setText("The AI has had enough of being blamed and has become sentient. All AI public transport is now refusing to transport anyone and causing chaos throughout the city");
+
 }
 
 //-------------- SUBCLASSES / YOUR DRAW CODE CAN GO HERE ---------------//
@@ -408,25 +454,25 @@ class ScenarioRoom extends PNGRoom {
       push();
 
       // title text
-      fill(255);
+      fill(0);
       textAlign(LEFT);
       textFont(headlineFont);
-      textSize(36);
+      textSize(40);
 
-      text("How do we feel?", this.drawX , 60);
+      text("Score: ???", this.drawX , 60);
 
       // title text
-      textSize(30);
+      textSize(40);
 
-      text(this.titleText, this.drawX , this.drawY);
+      text(this.titleText, this.drawX , this.drawY, 1000, 500);
      
       // Draw text in a box
       //text(this.titleText, width/6, height/6, this.textBoxWidth, this.textBoxHeight );
     
       textFont(bodyFont);
-      textSize(24);
+      textSize(40);
 
-      text(this.bodyText, this.drawX , this.drawY + 60, width - (this.drawX*2),height - (this.drawY+100) );
+      text(this.bodyText, this.drawX , this.drawY + 60, width/2,height/2 );
       
       pop();
     }
